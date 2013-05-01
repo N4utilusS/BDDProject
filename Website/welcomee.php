33713520f6265bc20aa3ebcfe28bc23d27a76b36
SuperPignouf <auplisni@ulb.ac.be>
2013-05-01 21:22:40 +0200

    lol

---
+++ Website/welcomee.php
@@ -0,0 +1,30 @@
+<!DOCTYPE  html>
+
+<?php session_start();?>
+
+<html>
+	<head>
+		<?php include("head.php"); ?>
+	</head>
+	
+	<body>
+	
+		<header> //En-tête
+			<h1>Welcome <?php echo $_SESSION['email']; ?></h1>
+		</header>
+		
+		<section> //Zone centrale
+			<p>Ca c'est notre site !</p>			
+		</section>	
+		
+		<nav> //Menu
+			<?php include ("menu.php"); ?>
+		</nav>
+		
+		<footer> //Footer !
+			<?php include ("footer.php"); ?>
+		</footer>
+		
+	</body>
+
+</html>
